d304fde163ec1f60ed470d8ba93bb0ba6b5fd309
DavideTarantino <davidetarantino2005@gmail.com>
2024-03-05 13:15:01 +0100

    query con join n.2

--- index.php
+++ index.php
@@ -96,3 +96,13 @@ FROM `students`
 JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+
+/*Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze*/
+
+SELECT *
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
+AND `degrees`.`level` = 'Magistrale';
